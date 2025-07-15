fb1e4af1bbd3e49049e17a4e9ec2c9949e615673
Sudhir <sudhiraptron@gmail.com>
2025-07-15 11:02:11 +0530

    15-07-2025

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-console.log("This is a message from the external script!");
+//  var  a = 10;
+
+// let a=30;
+//  a = 50;
+
+// const a = 20;
+
+// a = 40; // This will throw an error because 'a' is a constant
+
+
+// let a = 10;
+// a=50;
+
+
+
+// console.log(a);
+
+
+// console.log(a);
+
+// var a = 10;
+
---
+++ js/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+
+console.log("Hello, World!");
+console.log("This is a JavaScript file.");
+ console.log("This line will not execute because of the error above.");
