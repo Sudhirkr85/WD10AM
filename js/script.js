dd609c743a56dad3ea829eb7d4cc6c7c3ac58ae0
Sudhir <sudhiraptron@gmail.com>
2025-07-16 11:15:57 +0530

    16-07-2025

--- js/script.js
+++ js/script.js
@@ -7,16 +7,60 @@
 
 // a = 40; // This will throw an error because 'a' is a constant
 
-
 // let a = 10;
 // a=50;
 
-
-
 // console.log(a);
-
 
 // console.log(a);
 
 // var a = 10;
 
+// console.log(2**4);
+
+// a= a+1
+//  let b=++a;
+// let b=a++;
+
+// console.log(a);
+// console.log(b);
+
+// let a=15;
+// let b=20;
+// let c= a++ + ++b + ++a + b++ + a++ + ++b;
+
+// console.log(c);
+// console.log(a);
+// console.log(b);
+
+// let a=40;
+// let b="40"
+
+// console.log(a!=b);
+
+// console.log(a!==b);
+
+// console.log(a==b);
+// console.log(a===b);
+
+// console.log(10<20);
+
+// console.log(false | true);
+
+// console.log(13<<3);
+
+
+// console.log("" && null && 15 && 0 && 5);
+
+
+// console.log("Hello" || "World" || 0 || false || null || undefined|| "");
+
+// console.log( false || 0 || "" || null || undefined || "Hello" || "World" || 5);
+
+
+// console.log(!0);
+
+
+// console.log(!"Hello" && !0 && !null && !undefined && !5 && !"World" && !false);
+
+ console.log(!!"Hello");
--- js/tempCodeRunnerFile.js
+++ js/tempCodeRunnerFile.js
@@ -1,4 +1,1 @@
-
-console.log("Hello, World!");
-console.log("This is a JavaScript file.");
- console.log("This line will not execute because of the error above.");
+);
