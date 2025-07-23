b921ca6314b543c7b9e7b0366a409f5b61b04ed5
Sudhir <sudhiraptron@gmail.com>
2025-07-23 11:00:36 +0530

    23-07-2025

--- js/controlStatement.js
+++ js/controlStatement.js
@@ -1,9 +1,6 @@
-
-
 // if(100==="100") {
 //     console.log("100 is equal to 100");
 // }
-
 
 // if(100 === 100) {
 //     console.log("100 is equal to 100");
@@ -26,4 +23,56 @@
 // else {
 //     console.log("Number is not divisible by 2, 3 or 5");
 // }
+
+// Switch Statement
+
+// let a = 1;
+// switch (a) {
+//   case 1:
+//     console.log("Case 1");
+//     break;
+//   case 2:
+//     console.log("Case 2");
+//     break;
+//   default:
+//     console.log("Invalid input");
+// }
+
+// Loop Statement
+
+// for(let i=1;i<=10;i++){
+//     console.log(i);
+
+// }
+
+// let i = 1;
+
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
+
+// let i=1;
+
+// do{
+//     console.log(i);
+//     i++;
+// }
+// while(i<=10)
+
+//Jump Statement
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+//   if (i == 4) {
+//     break;
+//   }
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//   if (i >3 && i<7) continue;
+//   console.log(i);
+// }
+
+
 
---
+++ js/function.js
@@ -0,0 +1,51 @@
+//Function
+
+// sum(10, 20);
+
+// function sum(a, b) {
+//   console.log(a + b);
+// }
+
+// function subs(x, y) {
+//   console.log(x - y);
+// }
+
+// subs(50, 30);
+
+// checkNumberOddAndEven(50)
+
+// function checkNumberOddAndEven(n) {
+//   if (n % 2 == 0) {
+//     console.log("Number is Even");
+//   } else {
+//     console.log("Number is Odd");
+//   }
+// }
+
+
+
+// function checkNumberOddAndEven(n) {
+//   if (n % 2 !=0) {
+//     console.log(n+" is Odd");
+//   } else {
+//     console.log(n+" is Even");
+//   }
+// }
+// checkNumberOddAndEven(15)
+
+// checkNumberOddAndEven(50)
+
+
+// let a=10;
+// let b=20;
+// let c =a+b
+// console.log("a="+a);
+
+// console.log(a + " + "+ b + "  = "+ c);
+
+// console.log(`${a} +  ${b} = ${c}`);
+
+// console.log(`Sum of ${a} and ${b} =${c}`)
+
+// console.log("Sum of "+ a+ "and" + b +" = "+c)
+
