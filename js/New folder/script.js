7c8b65b2c2cc9be90ea7c79497f24e6d55f541f4
Sudhir <sudhiraptron@gmail.com>
2025-08-04 11:04:30 +0530

    04-08-2025

---
+++ js/New folder/script.js
@@ -0,0 +1,17 @@
+// let objH1=document.getElementsByTagName('h1')
+
+// console.log(objH1);
+
+// let h1=document.getElementById("s")
+
+// console.log(h1);
+
+// h1.style.color='red'
+// h1.style.display="none"
+
+// 
+
+let chnageColor=()=> {
+  const h1 = document.getElementById("s");
+  h1.style.color = "red";
+}
