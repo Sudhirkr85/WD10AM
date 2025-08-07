9d68c5dbb009c33497d0205039a567b7abf0a47c
Sudhir <sudhiraptron@gmail.com>
2025-08-07 11:01:37 +0530

    07-08-2025

---
+++ js/New folder/date.js
@@ -0,0 +1,11 @@
+// console.log(new Date());
+
+// let date = new Date("2025-07-02");
+let date = new Date();
+
+console.log(date);
+console.log(date.getDate());
+console.log(date.getDay());
+console.log(date.getTime());
+console.log(date.getFullYear());
+console.log(date.getHours());
