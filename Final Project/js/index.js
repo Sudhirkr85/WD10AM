a2987920ed927a9854e3b5a30342ee04982ea181
Sudhir <sudhiraptron@gmail.com>
2025-08-28 11:57:44 +0530

    28-08-2025

--- Final Project/js/index.js
+++ Final Project/js/index.js
@@ -1,113 +1,52 @@
-let products = document.querySelector(".products");
+async function getData() {
+  let res = await fetch("https://dummyjson.com/products");
+  let productsData = await res.json();
+  let data = productsData.products;
+  onLoad(data);
 
-let data = [
-  {
-    id: 1,
-    image:
-      "https://rukminim2.flixcart.com/image/110/110/xif0q/smartwatch/5/v/s/-original-imagxrhetgfuebnn.jpeg?q=80",
-    title: "Armani Exchange",
-    discription: "Men Analogue Watch",
-    o_price: 999,
-    d_price: 1999,
-    discount: 30,
-  },
-  {
-    id: 2,
-    image:
-      "https://rukminim2.flixcart.com/image/110/110/xif0q/projector/q/7/6/i9-pro-10-ei9027-led-projector-egate-original-imah5e3bggu5qcgp.jpeg?q=80",
-    title: "Armani Exchange",
-    discription: "Men Analogue Watch",
-    o_price: 999,
-    d_price: 1999,
-    discount: 30,
-  },
-  {
-    id: 3,
-    image:
-      "https://rukminim2.flixcart.com/image/110/110/xif0q/monitor/e/m/s/sa272-p1-full-hd-27-2025-um-hs2si-101-acer-original-imahey8jdh87j8kg.jpeg?q=80",
-    title: "Armani Exchange",
-    discription: "Men Analogue Watch",
-    o_price: 999,
-    d_price: 1999,
-    discount: 30,
-  },
-  {
-    id: 4,
-    image:
-      "https://rukminim2.flixcart.com/image/110/110/xif0q/dslr-camera/8/q/r/compact-camera-portable-small-point-and-shoot-digital-camera-for-original-imahec6vcssf4hzg.jpeg?q=80",
-    title: "Armani Exchange",
-    discription: "Men Analogue Watch",
-    o_price: 999,
-    d_price: 1999,
-    discount: 30,
-  },
-  {
-    id: 1,
-    image:
-      "https://rukminim2.flixcart.com/image/110/110/xif0q/smartwatch/5/v/s/-original-imagxrhetgfuebnn.jpeg?q=80",
-    title: "Armani Exchange",
-    discription: "Men Analogue Watch",
-    o_price: 999,
-    d_price: 1999,
-    discount: 30,
-  },
-  {
-    id: 2,
-    image:
-      "https://rukminim2.flixcart.com/image/110/110/xif0q/projector/q/7/6/i9-pro-10-ei9027-led-projector-egate-original-imah5e3bggu5qcgp.jpeg?q=80",
-    title: "Armani Exchange",
-    discription: "Men Analogue Watch",
-    o_price: 999,
-    d_price: 1999,
-    discount: 30,
-  },
-  {
-    id: 3,
-    image:
-      "https://rukminim2.flixcart.com/image/110/110/xif0q/monitor/e/m/s/sa272-p1-full-hd-27-2025-um-hs2si-101-acer-original-imahey8jdh87j8kg.jpeg?q=80",
-    title: "Armani Exchange",
-    discription: "Men Analogue Watch",
-    o_price: 999,
-    d_price: 1999,
-    discount: 30,
-  },
-  {
-    id: 4,
-    image:
-      "https://rukminim2.flixcart.com/image/110/110/xif0q/dslr-camera/8/q/r/compact-camera-portable-small-point-and-shoot-digital-camera-for-original-imahec6vcssf4hzg.jpeg?q=80",
-    title: "Armani Exchange",
-    discription: "Men Analogue Watch",
-    o_price: 999,
-    d_price: 1999,
-    discount: 30,
-  },
-];
-
-let items = ``;
-data.forEach((item) => {
-  items += `
-         <div class="product">
-          <img
-            src="${item.image}"
-          />
-          <div class="product-details">
-            <h3>${item.title}</h3>
-            <h4>${item.discription}</h4>
-            <div class="product-price">
-              <span>${item.o_price}</span>
-              <span class="d-price">${item.d_price}</span>
-              <span>(${item.discount}%)</span>
-            </div>
-            <button class="add-to-bag" onclick="addToCart()">Add Bag</button>
-           
+  function onLoad(data) {
+    let products = document.querySelector(".products");
+ 
+    
+    
+    let cart = JSON.parse(localStorage.getItem('cart'))
+     document.querySelector(".bag-count").textContent = cart.length;
+    let items = ``;
+    data.forEach((item) => {
+      items += `
+       <div class="product">
+        <img
+          src="${item.thumbnail}"
+        />
+        <div class="product-details">
+          <h3>${item.title}</h3>
+          <h4>${item.description}</h4>
+          <div class="product-price">
+            <span>${item.price}</span>
+             <span>(${item.discountPercentage}%)</span>
           </div>
-        </div>`;
-});
+          <button class="add-to-bag" data-id="${item.id}" >Add Bag</button>
 
-products.innerHTML = items;
+        </div>
+      </div>`;
+    });
 
-function addToCart() {
-  let countitem = document.querySelector(".bag-count");
-  let count = countitem.textContent;
-  countitem.textContent = ++count;
+    products.innerHTML = items;
+
+    const buttons = document.querySelectorAll(".add-to-bag");
+
+    buttons.forEach((button) => {
+      button.addEventListener("click", () => {
+        let id = button.getAttribute("data-id");
+
+        if (!cart.includes(id)) {
+          cart.push(id);
+          document.querySelector(".bag-count").textContent = cart.length;
+          localStorage.setItem("cart",JSON.stringify(cart));
+        }
+      });
+    });
+  }
 }
+
+getData();
